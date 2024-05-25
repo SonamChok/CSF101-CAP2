be11a746b30d3e72572a5f391e05bde7842da392
unknown <02230272.cst@rub.edu.bt>
2024-05-25 10:17:29 -0700

    CAP2

--- CAP2_02230272.py
+++ CAP2_02230272.py
@@ -1,13 +1,3 @@
-################################
-# Sonam Choki
-# 1ME
-# 02230272
-################################
-# REFERENCES
-# https://www.scribd.com/document/457278570/Code-for-Bank-Application
-#https://www.geeksforgeeks.org/python-program-to-create-bankaccount-class-with-deposit-withdraw-function/
-################################
-# Read the input.accounts.txt
 import random
 
 class Account:
@@ -87,75 +77,70 @@ class Bank:
         else:
             return "Invalid sender or receiver account number."
 
+bank = Bank()
+bank.load_accounts()
 
-def main():
-    bank = Bank()
-    bank.load_accounts()
+while True:
+    print("\nWelcome to the Banking Application")
+    print("1. Create Account")
+    print("2. Login")
+    print("3. Exit")
 
-    while True:
-        print("\nWelcome to the Banking Application")
-        print("1. Create Account")
-        print("2. Login")
-        print("3. Exit")
+    choice = input("Enter your choice: ")
 
-        choice = input("Enter your choice: ")
+    if choice == "1":
+        account_type = input("Enter account type (Personal/Business): ").capitalize()
+        print(bank.create_account(account_type))
 
-        if choice == "1":
-            account_type = input("Enter account type (Personal/Business): ").capitalize()
-            print(bank.create_account(account_type))
+    elif choice == "2":
+        account_number = int(input("Enter account number: "))
+        password = input("Enter password: ")
+        account = bank.login(account_number, password)
+        if isinstance(account, Account):
+            print(f"Login successful. Account balance: {account.balance}")
+            while True:
+                print("\nAccount Menu")
+                print("1. Deposit")
+                print("2. Withdraw")
+                print("3. Transfer Money")
+                print("4. Delete Account")
+                print("5. Logout")
 
-        elif choice == "2":
-            account_number = int(input("Enter account number: "))
-            password = input("Enter password: ")
-            account = bank.login(account_number, password)
-            if isinstance(account, Account):
-                print(f"Login successful. Account balance: {account.balance}")
-                while True:
-                    print("\nAccount Menu")
-                    print("1. Deposit")
-                    print("2. Withdraw")
-                    print("3. Transfer Money")
-                    print("4. Delete Account")
-                    print("5. Logout")
+                account_choice = input("Enter your choice: ")
 
-                    account_choice = input("Enter your choice: ")
+                if account_choice == "1":
+                    amount = float(input("Enter deposit amount: "))
+                    print(account.deposit(amount))
 
-                    if account_choice == "1":
-                        amount = float(input("Enter deposit amount: "))
-                        print(account.deposit(amount))
+                elif account_choice == "2":
+                    amount = float(input("Enter withdrawal amount: "))
+                    print(account.withdraw(amount))
 
-                    elif account_choice == "2":
-                        amount = float(input("Enter withdrawal amount: "))
-                        print(account.withdraw(amount))
+                elif account_choice == "3":
+                    receiver_account_number = int(input("Enter receiver's account number: "))
+                    amount = float(input("Enter transfer amount: "))
+                    print(bank.transfer_money(account.account_number, receiver_account_number, amount))
 
-                    elif account_choice == "3":
-                        receiver_account_number = int(input("Enter receiver's account number: "))
-                        amount = float(input("Enter transfer amount: "))
-                        print(bank.transfer_money(account.account_number, receiver_account_number, amount))
-
-                    elif account_choice == "4":
-                        confirmation = input("Are you sure you want to delete your account? (yes/no): ").lower()
-                        if confirmation == "yes":
-                            print(account.delete_account())
-                            del bank.accounts[account_number]
-                            bank.save_accounts()
-                            break
-
-                    elif account_choice == "5":
+                elif account_choice == "4":
+                    confirmation = input("Are you sure you want to delete your account? (yes/no): ").lower()
+                    if confirmation == "yes":
+                        print(account.delete_account())
+                        del bank.accounts[account_number]
+                        bank.save_accounts()
                         break
 
-                    else:
-                        print("Invalid choice.")
+                elif account_choice == "5":
+                    break
 
-            else:
-                print(account)
-
-        elif choice == "3":
-            print("Exiting...")
-            break
+                else:
+                    print("Invalid choice.")
 
         else:
-            print("Invalid choice.")
+            print(account)
 
-if __name__ == "__main__":
-    main()
+    elif choice == "3":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice.")
